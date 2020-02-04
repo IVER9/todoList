73f938700b05dc56f11eb9cd57ebc316fbcf93a7
IVER9 <answer48694063@outlook.jp>
2020-02-04 22:35:20 +0900

    データベースに連携にトランザクションをはりました

--- model/todo.php
+++ model/todo.php
@@ -13,6 +13,7 @@ class Todo {
             $this->db = new PDO(DSN, USERNAME, PASSWORD);
             $this->db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
+            $this->db->rollBack();
             exit($e->getMessage());
         }
     }
@@ -21,6 +22,16 @@ class Todo {
     }
     public function getId() {
         return $this->id;
+    }
+    public function checkId($id){
+        $sql = "SELECT * FROM todo WHERE id='$id'";
+        $result = $this->findByQuery($sql);
+        if (!$result) {
+            return false;
+        }
+        foreach ($result as $value) {
+            return $value['id'];
+        }
     }
     public function setTitle($title) {
         $this->title = $title;
@@ -40,26 +51,33 @@ class Todo {
     public function getStatus() {
         return $this->status;
     }
-
     public function findByQuery($sql) {
+        $this->db->beginTransaction();
         $statement = $this->db->prepare($sql);
         $statement->execute();
+        $this->db->commit();
         return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
     public function save() {
+        $this->db->beginTransaction();
         $sql = "INSERT INTO todo (title, detail, status, created_at) VALUES ('$this->title', '$this->detail', '0', NOW())";
         $statement = $this->db->prepare($sql);
         $statement->execute();
+        $this->db->commit();
     }
-    public function deleted() {
+    public function delete() {
+        $this->db->beginTransaction();
         $sql = "DELETE FROM todo WHERE (id = $this->id)";
         $statement = $this->db->prepare($sql);
         $statement->execute();
+        $this->db->commit();
     }
     public function completion() {
+        $this->db->beginTransaction();
         $sql = "UPDATE todo SET status = '$this->status' WHERE (id= '$this->id')";
         $statement = $this->db->prepare($sql);
         $statement->execute();
+        $this->db->commit();
     }
 }
  ?>
